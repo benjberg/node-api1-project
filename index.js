c604c3edb60a41b2ddd40b7e587b5893530e4b7b
Ben <bbjberg91@gmail.com>
2020-04-06 17:46:31 -0400

    mvp working

--- index.js
+++ index.js
@@ -41,12 +41,41 @@ res.status(400).json({message: 'Please provide name and bio for the user' })
      };
 })
 
+server.put('/api/users/:id', (req, res) =>{
+    const id= req.params.id;
+    const newUser = req.body;
+    const user = users.find((user)=> user.id === id);
+    if(user){
+        if(newUser.name && newUser.bio){
+            try{
+                users= users.map((user) =>{
+                    if (user.id === id){
+                        return{
+                            ...user,
+                            name: newUser.name,
+                            bio: newUser.bio
+                        }
+                    }else {
+                        return user
+                    }
+                });
+                res.status(200).json(users);
+            }catch(error) {
+                res.status(500).json({message: 'The user information could not be modified'})
+            }
+        }else {
+            res.status(400).json({ message: 'Please provide name and bio for the user' })
+        }
+    }else {
+        res.status(404).json({ message: 'The user with the specified ID does not exist' })
+    }
+})
 server.delete('/api/users/:id', (req,res) =>{
     const reqId = req.params.id;
     let user= users.filter(user =>{
         return user.id === reqId;
     })[0];
-const index= users.indexOf(user);
+    const index= users.indexOf(user);
     users.splice(index, 1);
     res.json({message: `user ${reqId} deleted `})
 })
